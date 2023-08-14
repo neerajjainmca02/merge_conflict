f5b6b31def9e3e4ac4acfa2e4b771e0935a385f8
Ubuntu <ubuntu@ip-172-31-88-163.ec2.internal>
2023-08-14 09:58:46 +0000

    merged conflicts and committing file

--- functions.cs
+++ functions.cs
@@ -2,8 +2,16 @@ Initial function()
 {	
  get connection....
 }
+
 # code written by Developer 1
 Initiate function()
 {
 	write string .....
 } 
+
+# code written by developer 2
+Initiate function2()
+{
+	read string .....
+}
+
